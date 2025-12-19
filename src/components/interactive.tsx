81d236a2c7da96bed27dbabcc2fa313afcbb659b
jacksonmalloy <jacksmalloy@gmail.com>
2025-12-19 09:15:39 -0800

    official emails

--- src/components/interactive.tsx
+++ src/components/interactive.tsx
@@ -211,7 +211,7 @@ export const AppStoreButtons = () => {
               style={{ display: "block", textDecoration: "none" }}
             >
               <Img
-                src="https://cdn.trckd.ca/assets/Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+                src="https://cdn.trckd.ca/assets/Download_on_the_App_Store_Badge_US-UK_blk_092917.png"
                 alt="Download on the App Store"
                 height="36"
                 style={{ display: "block" }}
@@ -224,7 +224,7 @@ export const AppStoreButtons = () => {
               style={{ display: "block", textDecoration: "none" }}
             >
               <Img
-                src="https://cdn.trckd.ca/assets/GetItOnGooglePlay_Badge_Web_color_English.svg"
+                src="https://cdn.trckd.ca/assets/GetItOnGooglePlay_Badge_Web_color_English.png"
                 alt="Get it on Google Play"
                 height="36"
                 style={{ display: "block" }}
